e9bd310b2b8bb63a901a0c17364d901e92044fa1
pranshubisht9@gmail.com <pranshubisht9@gmail.com>
2023-07-20 11:29:31 +0530

    addingg learning 1

--- _01_SpringBootApp/src/main/java/com/securityapp/Application.java
+++ _01_SpringBootApp/src/main/java/com/securityapp/Application.java
@@ -1,4 +1,4 @@
-package com.learning.app;
+package com.securityapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- _01_SpringBootApp/src/main/java/com/securityapp/controller/MyController.java
+++ _01_SpringBootApp/src/main/java/com/securityapp/controller/MyController.java
@@ -1,4 +1,4 @@
-package com.learning.app.controller;
+package com.securityapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +10,10 @@ public class MyController {
     public String sayHello(){
         return "Welcome to the Spring Security";
     }
+
+    @GetMapping("/hello2")
+    public String sayHello2(){
+        return "Welcome to the Spring Security 2";
+    }
+
 }
